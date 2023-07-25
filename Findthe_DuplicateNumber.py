c1c6817c5e974555c4ab8be6a8513f57ca457203
Kadir-Akipek <110154102+Kadir-Akipek@users.noreply.github.com>
2023-07-25 11:45:12 +0300

    solution

---
+++ Findthe_DuplicateNumber.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slowPointer = 0
+        fastPointer = 0
+        while True:
+            slowPointer = nums[slowPointer]
+            fastPointer = nums[nums[fastPointer]]
+            if slowPointer == fastPointer:
+                break
+        
+        secondSlowPointer = 0
+        
+        while True:
+            slowPointer = nums[slowPointer]
+            secondSlowPointer = nums[secondSlowPointer]
+            if slowPointer == secondSlowPointer:
+                return slowPointer
---
+++ Intersectionof_TwolinkedLists.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        firstPointer = headA
+        secondPointer = headB
+
+        while firstPointer != secondPointer:
+            firstPointer = firstPointer.next if firstPointer != None else headB
+            secondPointer = secondPointer.next if secondPointer != None else headA
+
+        return firstPointer
